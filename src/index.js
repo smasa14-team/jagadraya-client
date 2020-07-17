a2a8375edd693b050bd1c0188bba98501bfa189a
smartoryu <prikenang@gmail.com>
2020-07-17 23:03:17 +0700

    🎊added packages and redux boilerplate

--- src/index.js
+++ src/index.js
@@ -1,14 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Provider } from "react-redux";
+import { createStore, applyMiddleware } from "redux";
+import Thunk from "redux-thunk";
+import rootReducer from "./redux/reducers";
+
+import "./styles/index.scss";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+
+const init_state = {};
+const store = createStore(rootReducer, init_state, applyMiddleware(Thunk));
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
